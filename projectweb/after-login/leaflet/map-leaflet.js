a0a310ead267e5227240e8c000c684cdd4d94127
xristos98 <xristosdemetriou98@hotmail.com>
2023-07-20 20:35:26 +0300

    a,b

--- projectweb/after-login/leaflet/map-leaflet.js
+++ projectweb/after-login/leaflet/map-leaflet.js
@@ -4,87 +4,117 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// map.locate({ setView: true, watch: true, maxZoom: 16 });
-// function onLocationFound(e) {
-//     var radius = e.accuracy / 2;
+map.locate({ setView: true,/* watch: true,*/ maxZoom: 16 });
+function onLocationFound(e) {
+    var radius = e.accuracy / 2;
   
-//     L.marker(e.latlng).addTo(map).bindPopup("You are here").openPopup();
-//     L.circle(e.latlng, radius).addTo(map);
-//   }
+    L.marker(e.latlng).addTo(map).bindPopup("You are here").openPopup();
+    L.circle(e.latlng, radius).addTo(map);
+  }
   
-//   // Handle location error event
-//   function onLocationError(e) {
-//     alert(e.message);
-//   }
+  // Handle location error event
+  function onLocationError(e) {
+    alert(e.message);
+  }
   
-//   // Listen for location found and error events
-//   map.on('locationfound', onLocationFound);
-//   map.on('locationerror', onLocationError);
+  // Listen for location found and error events
+  map.on('locationfound', onLocationFound);
+  map.on('locationerror', onLocationError);
 
 L.control.locate().addTo(map);
+
+
+var markerDataList = [];
+
+// Define the filterMarkers function outside the AJAX success function
+function filterMarkers(name) {
+    // Remove all markers from the map
+    map.eachLayer(function (layer) {
+        if (layer instanceof L.Marker) {
+            map.removeLayer(layer);
+        }
+    });
+
+    // If the search name is empty, filter the markers to include only those with a discount
+    // Otherwise, filter the markers based on the search name
+    var filteredMarkers = markerDataList.filter(function (markerData) {
+        if (name === '') {
+            return markerData.discount;
+        } else {
+            return markerData.poi_name && markerData.poi_name.toLowerCase().includes(name.toLowerCase());
+        }
+    });
+
+    // Add the filtered markers to the map
+    filteredMarkers.forEach(function (markerData) {
+        var markerColor = markerData.discount ? 'green' : 'red';
+        var markerIcon = L.ExtraMarkers.icon({
+            markerColor: markerColor,
+        });
+        L.marker([markerData.lat, markerData.lng], { icon: markerIcon })
+            .bindPopup(markerData.poi_name + '<br>' + markerData.name + (markerData.discount ? ('<br>Discount: ' + markerData.discount) : ''))
+            .addTo(map);
+    });
+}
+
+
+
+// Define the click event handler for the #btn_search button outside the filterMarkers function
+$('#btn_search').on("click", function () {
+    var searchName = $('#search').val();
+    filterMarkers(searchName);
+});
+
+//Na do gia na antikatastiso ta pano 
+$('#btn_search').on("click", function () {
+    var searchName = $('#search').val();
+    var selectedCategory = $('#category_select').val();
+    filterMarkers(searchName, selectedCategory);
+});
+
+$('#category_select').on("change", function () {
+    var searchName = $('#search').val();
+    var selectedCategory = $('#category_select').val();
+    filterMarkers(searchName, selectedCategory);
+});
+
+function filterMarkers(name, category) {
+    // Remove all markers from the map
+    for(var i = 0; i < markers.length; i++){
+        map.removeLayer(markers[i]);
+    }
+
+    // Filter the markers based on the search name, the selected category, and whether they have a discount
+    for (var i = 0; i < markerList.length; i++) {
+        var marker = markerList[i];
+        var markerName = marker.name.toLowerCase();
+        var markerCategory = marker.category.toLowerCase();
+        var markerHasDiscount = marker.discount > 0;
+
+        if ((name === "" || markerName.includes(name.toLowerCase())) &&
+            (category === "" || markerCategory.includes(category.toLowerCase())) &&
+            (name !== "" || markerHasDiscount)) {
+            var markerColor = markerHasDiscount ? 'green' : 'red';
+            var markerIcon = L.ExtraMarkers.icon({
+                markerColor: markerColor,
+            });
+            markers.push(L.marker([marker.lat, marker.lng], {icon: markerIcon}).addTo(map));
+        }
+    }
+} */
+
+
 
 
 $.ajax({
     url: 'fetch_markers.php',
     dataType: 'json',
     success: function (data) {
-        
-        // var filteredMarkers = data;
-        // function filterMarkers(name) {
-        //     map.eachLayer(function (layer) {
-        //         if (layer instanceof L.Marker) {
-        //             map.removeLayer(layer);
-        //         }
-        //     });
+        // Store the data into the markerDataList variable
+        markerDataList = data;
 
-        //     var filteredMarkers = markerDataList.filter(function (markerData) {
-        //         return markerData.name && markerData.name.toLowerCase().includes(name.toLowerCase());
-        //     });
-
-        //     $('#btn_search').on("click", function () {
-        //         var searchName = $('#search').val();
-        //         filterMarkers(searchName);
-        //     });
-          
-        var markerDataList = data;
-        var greenMarkers = [];
-        var redMarkers = [];
-
-        markerDataList.forEach(function (markerData) {
-            var poi_name = markerData.poi_name;
-            var prod_name = markerData.name;
-            var lat = markerData.lat;
-            var lng = markerData.lng;
-            var discount = markerData.discount;
-
-            var markerIcon = discount ? L.ExtraMarkers.icon({
-                markerColor: 'green', // Set the marker color to green for supermarkets with offers
-            }) : L.ExtraMarkers.icon({
-                markerColor: 'red', // Set the marker color to red for supermarkets without offers
-            });
-
-            var marker = L.marker([lat, lng], { icon: markerIcon });
-
-            // Use a different condition to handle the popup content
-            if (discount) {
-                marker.bindPopup(poi_name + '<br>' + prod_name + '<br>Discount: ' + discount);
-                greenMarkers.push(marker);
-            } else {
-                marker.bindPopup(poi_name);
-                redMarkers.push(marker);
-            }
-        });
-
-        // Add the red markers first to maintain the order on the map
-        redMarkers.forEach(function (marker) {
-            marker.addTo(map);
-        });
-
-        greenMarkers.forEach(function (marker) {
-            marker.addTo(map);
-        });
-    //}
-    // filterMarkers('');   
+        // Call filterMarkers after you've fetched and processed the marker data
+        filterMarkers('');
     }
 });
 
--- projectweb/after-login/main.php
+++ projectweb/after-login/main.php
@@ -11,6 +11,18 @@ if (isset($_SESSION['loggedin'])) {
     header('Location: ../index');
     exit();
 }
+
+/* Connect to your database
+$db = new PDO('mysql:host=localhost;dbname=projectweb;charset=utf8', 'root', ' ');
+
+// Query the database for the categories
+$result = $db->query('SELECT name FROM categories');
+
+// Fetch the categories as an associative array
+$categories = $result->fetchAll(PDO::FETCH_ASSOC);
+*/
+
+
 ?>
 
 <!DOCTYPE html>
@@ -22,13 +34,34 @@ if (isset($_SESSION['loggedin'])) {
     <link href="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.79.0/dist/L.Control.Locate.min.css" rel="stylesheet">
     <link rel="stylesheet" href="leaflet/dist/css/leaflet.extra-markers.min.css">
     
-    
+
 </head>
 <body>
     <ul>
         <li><a href="logout.php">Logout</a></li>
         <li><a href="profile.html">Profile</a></li>
         <li class = "left"><a >Hi, <?php echo htmlspecialchars($_SESSION["username"]); ?></a></li>
+        <select id="category_select">
+        <option value="">All Categories</option>
+        <!--<option value="Groceries">Groceries</option>
+        <option value="Electronics">Electronics</option>
+        <option value="Clothing">Clothing</option>
+        <option value="Books">Books</option> -->
+
+        <!-- li>
+            <form autocomplete="off" method="POST" id="search-form" onsubmit="return false;"> 
+            <input type="text" id="search" placeholder="Search..."> <style> #search{margin-top:13px;}</style>
+            Add the new dropdown select for the product categories here 
+            <select id="category_select">
+              <option value="">All Categories</option>
+              <?php foreach ($categories as $category): ?>
+                <option value="<?php echo htmlspecialchars($category['category_name']); ?>">
+                  <?php echo htmlspecialchars($category['category_name']); ?>
+                </option>
+              <?php endforeach; ?>
+            </select> -->
+
+    </select>
         <li>
             <form autocomplete="off" method="POST" id="search-form" onsubmit="return false;"> 
             <input type="text" id="search" placeholder="Search..."> <style> #search{margin-top:13px;}</style>
