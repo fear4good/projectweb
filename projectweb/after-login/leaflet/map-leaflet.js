5ad495a41753eb93e252576c798d92caa8decc7f
fear4good <83022667+fear4good@users.noreply.github.com>
2023-07-20 16:19:41 +0300

    updated markers
    markers with offers show as green, without red

--- projectweb/after-login/leaflet/map-leaflet.js
+++ projectweb/after-login/leaflet/map-leaflet.js
@@ -1,25 +1,25 @@
-var map = L.map('map').setView([0, 0], 13);
+var map = L.map('map').setView([38.2466, 21.7346], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-map.locate({ setView: true, watch: true, maxZoom: 16 });
-function onLocationFound(e) {
-    var radius = e.accuracy / 2;
+// map.locate({ setView: true, watch: true, maxZoom: 16 });
+// function onLocationFound(e) {
+//     var radius = e.accuracy / 2;
   
-    L.marker(e.latlng).addTo(map).bindPopup("You are here").openPopup();
-    L.circle(e.latlng, radius).addTo(map);
-  }
+//     L.marker(e.latlng).addTo(map).bindPopup("You are here").openPopup();
+//     L.circle(e.latlng, radius).addTo(map);
+//   }
   
-  // Handle location error event
-  function onLocationError(e) {
-    alert(e.message);
-  }
+//   // Handle location error event
+//   function onLocationError(e) {
+//     alert(e.message);
+//   }
   
-  // Listen for location found and error events
-  map.on('locationfound', onLocationFound);
-  map.on('locationerror', onLocationError);
+//   // Listen for location found and error events
+//   map.on('locationfound', onLocationFound);
+//   map.on('locationerror', onLocationError);
 
 L.control.locate().addTo(map);
 
@@ -28,44 +28,67 @@ $.ajax({
     url: 'fetch_markers.php',
     dataType: 'json',
     success: function (data) {
-        // Store the marker data globally
+        
+        // var filteredMarkers = data;
+        // function filterMarkers(name) {
+        //     map.eachLayer(function (layer) {
+        //         if (layer instanceof L.Marker) {
+        //             map.removeLayer(layer);
+        //         }
+        //     });
+
+        //     var filteredMarkers = markerDataList.filter(function (markerData) {
+        //         return markerData.name && markerData.name.toLowerCase().includes(name.toLowerCase());
+        //     });
+
+        //     $('#btn_search').on("click", function () {
+        //         var searchName = $('#search').val();
+        //         filterMarkers(searchName);
+        //     });
+          
         var markerDataList = data;
+        var greenMarkers = [];
+        var redMarkers = [];
 
-        // Function to filter markers based on name
-        function filterMarkers(name) {
-            // Clear existing markers from the map
-            map.eachLayer(function (layer) {
-                if (layer instanceof L.Marker) {
-                    map.removeLayer(layer);
-                }
+        markerDataList.forEach(function (markerData) {
+            var poi_name = markerData.poi_name;
+            var prod_name = markerData.name;
+            var lat = markerData.lat;
+            var lng = markerData.lng;
+            var discount = markerData.discount;
+
+            var markerIcon = discount ? L.ExtraMarkers.icon({
+                markerColor: 'green', // Set the marker color to green for supermarkets with offers
+            }) : L.ExtraMarkers.icon({
+                markerColor: 'red', // Set the marker color to red for supermarkets without offers
             });
 
-            // Filter the marker data based on the search name
-            var filteredMarkers = markerDataList.filter(function (markerData) {
-                return markerData.name  && markerData.name.toLowerCase().includes(name.toLowerCase());
-            });
+            var marker = L.marker([lat, lng], { icon: markerIcon });
 
-            // Add filtered markers to the map
-            filteredMarkers.forEach(function (markerData) {
-                var name = markerData.name;
-                var lat = markerData.lat;
-                var lng = markerData.lng;
-
-                var marker = L.marker([lat, lng]).addTo(map);
-                marker.bindPopup(name);
-            });
-        }
-
-        // Event listener for search button click
-        $('#btn_search').on("click", function () {
-            var searchName = $('#search').val();
-            filterMarkers(searchName);
+            // Use a different condition to handle the popup content
+            if (discount) {
+                marker.bindPopup(poi_name + '<br>' + prod_name + '<br>Discount: ' + discount);
+                greenMarkers.push(marker);
+            } else {
+                marker.bindPopup(poi_name);
+                redMarkers.push(marker);
+            }
         });
 
-        // Initial rendering of all markers
-        filterMarkers('');
+        // Add the red markers first to maintain the order on the map
+        redMarkers.forEach(function (marker) {
+            marker.addTo(map);
+        });
+
+        greenMarkers.forEach(function (marker) {
+            marker.addTo(map);
+        });
+    //}
+    // filterMarkers('');   
     }
 });
+
+
 
 
 
