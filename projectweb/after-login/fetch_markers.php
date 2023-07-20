d8a13246ebcf2fa0b8f59490b5f20d05773f35f7
fear4good <83022667+fear4good@users.noreply.github.com>
2023-07-20 16:18:58 +0300

    select pois with offers

--- projectweb/after-login/fetch_markers.php
+++ projectweb/after-login/fetch_markers.php
@@ -1,20 +1,30 @@
 <?php
-    include '../connect.php';
+include '../connect.php';
 
-    $markers = array(); 
-    $sql = "SELECT name, latitude, longitude FROM pois";
-    $result = $db->query($sql);
+$markers = array(); 
+$sql = "SELECT p.id, p.name AS poi_name, p.latitude AS lat, p.longitude AS lng, 
+               o.discount, pr.name AS prod_name, c.name AS category, sc.name AS subcategory
+        FROM pois p
+        LEFT JOIN offers o ON p.id = o.supermarket_id
+        LEFT JOIN products pr ON o.product_id = pr.id
+        LEFT JOIN subcategories sc ON pr.subcategory_id = sc.id
+        LEFT JOIN categories c ON sc.category_id = c.id";
+$result = $db->query($sql);
 
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $marker = array(
-                'name' => $row['name'],
-                'lat' => $row['latitude'],
-                'lng' => $row['longitude']
-            );
-            $markers[] = $marker;
-        }
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $marker = array(
+            'poi_name' => $row['poi_name'],
+            'name' => $row['prod_name'],
+            'lat' => $row['lat'],
+            'lng' => $row['lng'],
+            'discount' => $row['discount'],
+            'category' => $row['category'],
+            'subcategory' => $row['subcategory']
+        );
+        $markers[] = $marker;
     }
+}
 
-    echo json_encode($markers);
+echo json_encode($markers);
 ?>
