6fa60997de80ebb8cc292aa852943464b6af2d7e
fear4good <83022667+fear4good@users.noreply.github.com>
2023-07-22 00:02:45 +0300

    Update for categories
    Now include get functions for the category/subcategory search

--- projectweb/after-login/fetch_markers.php
+++ projectweb/after-login/fetch_markers.php
@@ -1,30 +1,47 @@
 <?php
-include '../connect.php';
+    include '../connect.php';
 
-$markers = array(); 
-$sql = "SELECT p.id, p.name AS poi_name, p.latitude AS lat, p.longitude AS lng, 
-               o.discount, pr.name AS prod_name, c.name AS category, sc.name AS subcategory
-        FROM pois p
-        LEFT JOIN offers o ON p.id = o.supermarket_id
-        LEFT JOIN products pr ON o.product_id = pr.id
-        LEFT JOIN subcategories sc ON pr.subcategory_id = sc.id
-        LEFT JOIN categories c ON sc.category_id = c.id";
-$result = $db->query($sql);
+    // Check if category and subcategory query parameters are provided
+    $category = $_GET['category'] ?? null;
+    $subcategory = $_GET['subcategory'] ?? null;
 
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        $marker = array(
-            'poi_name' => $row['poi_name'],
-            'name' => $row['prod_name'],
-            'lat' => $row['lat'],
-            'lng' => $row['lng'],
-            'discount' => $row['discount'],
-            'category' => $row['category'],
-            'subcategory' => $row['subcategory']
-        );
-        $markers[] = $marker;
+    // Build the SQL query based on whether category and subcategory are provided
+    $sql = "SELECT p.id, p.name AS poi_name, p.latitude AS lat, p.longitude AS lng, 
+                o.discount, pr.name AS prod_name, c.name AS category, sc.name AS subcategory
+            FROM pois p
+            LEFT JOIN offers o ON p.id = o.supermarket_id
+            LEFT JOIN products pr ON o.product_id = pr.id
+            LEFT JOIN subcategories sc ON pr.subcategory_id = sc.id
+            LEFT JOIN categories c ON sc.category_id = c.id";
+
+    if ($category && $subcategory) {
+        // Filter by both category and subcategory
+        $sql .= " WHERE c.id = '$category' AND sc.id = '$subcategory' AND o.discount IS NOT NULL";
+    } elseif ($category) {
+        // Filter only by category
+        $sql .= " WHERE c.id = '$category' AND o.discount IS NOT NULL";
+    } elseif ($subcategory) {
+        // Filter only by subcategory
+        $sql .= " WHERE sc.id = '$subcategory' AND o.discount IS NOT NULL";
     }
-}
 
-echo json_encode($markers);
+    $result = $db->query($sql);
+
+    $markers = array(); 
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $marker = array(
+                'poi_name' => $row['poi_name'],
+                'name' => $row['prod_name'],
+                'lat' => $row['lat'],
+                'lng' => $row['lng'],
+                'discount' => $row['discount'],
+                'category' => $row['category'],
+                'subcategory' => $row['subcategory']
+            );
+            $markers[] = $marker;
+        }
+    }
+
+    echo json_encode($markers);
 ?>
