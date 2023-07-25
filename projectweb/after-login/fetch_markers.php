d70a9e1a10e249db3ad14a52b4df9ee6d7768911
fear4good <83022667+fear4good@users.noreply.github.com>
2023-07-25 18:12:18 +0300

    updated fetch variables
    now includes the new variables of the table offers

--- projectweb/after-login/fetch_markers.php
+++ projectweb/after-login/fetch_markers.php
@@ -7,7 +7,8 @@
 
     // Build the SQL query based on whether category and subcategory are provided
     $sql = "SELECT p.id, p.name AS poi_name, p.latitude AS lat, p.longitude AS lng, 
-                o.discount, pr.name AS prod_name, c.name AS category, sc.name AS subcategory
+                o.id AS offer_id, o.discount, o.date, o.likes, o.dislikes, o.stock,
+                pr.name AS prod_name, c.name AS category, sc.name AS subcategory
             FROM pois p
             LEFT JOIN offers o ON p.id = o.supermarket_id
             LEFT JOIN products pr ON o.product_id = pr.id
@@ -32,10 +33,15 @@
         while ($row = $result->fetch_assoc()) {
             $marker = array(
                 'poi_name' => $row['poi_name'],
-                'name' => $row['prod_name'],
+                'prod_name' => $row['prod_name'],
                 'lat' => $row['lat'],
                 'lng' => $row['lng'],
+                'offer_id'=> $row['offer_id'],
                 'discount' => $row['discount'],
+                'date' => $row['date'],
+                'likes' => $row['likes'],
+                'dislikes' => $row['dislikes'],
+                'stock' => $row['stock'],
                 'category' => $row['category'],
                 'subcategory' => $row['subcategory']
             );
