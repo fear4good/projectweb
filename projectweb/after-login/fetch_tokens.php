fbaae32b703362f2110664675ce718ee0d503b06
fear4good <83022667+fear4good@users.noreply.github.com>
2023-07-27 23:46:22 +0300

    Update fetch_tokens.php

--- projectweb/after-login/fetch_tokens.php
+++ projectweb/after-login/fetch_tokens.php
@@ -1,21 +1,19 @@
 <?php
-session_start();
+
+include '../connect.php';
 
 $tokens = 0;
 
 if (isset($_SESSION['loggedin']) && $_SESSION['role'] != 'admin') {
     // Fetch tokens for the user for the current month
-    $mysqli = new mysqli('localhost', 'root', '', 'projectweb');
-    $monthYear = date('Y-m');
-    $stmt = $mysqli->prepare("SELECT tokens FROM tokens WHERE id = ? AND date_ = ?");
-    $stmt->bind_param("is", $_SESSION['id'], $monthYear);
+    $stmt = $db->prepare("SELECT tokens FROM tokens WHERE id = ?");
+    $stmt->bind_param("s", $_SESSION['id']);
     $stmt->execute();
     $result = $stmt->get_result();
     if ($row = $result->fetch_assoc()) {
         $tokens = $row['tokens'];
     }
     $stmt->close();
-    $mysqli->close();
 }
 
 echo json_encode(['tokens' => $tokens]);
